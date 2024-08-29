781e2015603d8f2c404448c850e3d85c31c5cfd0
Shao Zixuan <117291172+szxshawn@users.noreply.github.com>
2024-08-29 11:26:09 +0000

    上课晚上

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template,request
 
 app=Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["get","post"])
 def index():
     return render_template('index.html')
 
